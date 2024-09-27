ee8408fb55b9f0818767f8d42366b6e04d4e789b
jayawickrma <142202862+jayawickrma@users.noreply.github.com>
2024-09-27 21:18:49 +0530

    email option

--- fox-master/js/Script.js
+++ fox-master/js/Script.js
@@ -1,9 +1,18 @@
-function sendMail(){
-    let params={
-        email : document.getElementById("name").value(),
-        form_email :document.getElementById("email").value(),
-        subject : document.getElementById("subject").value(),
-        message : document.getElementById("message").value(),
-    }
-    emailjs.send("service_657ab6c","template_bfwxv2n",params).then(alert("Email sent !!!"));
+function sendMail() {
+    // Collect form data into params object
+    let params = {
+        to_name: document.getElementById("name").value,
+        from_name: document.getElementById("email").value,
+        subject: document.getElementById("subject").value,
+        message: document.getElementById("message").value,
+    };
+
+    // Send email using EmailJS
+    emailjs.send('service_657ab6c', 'template_v9j5mum', params)
+        .then(function(response) {
+            alert('Message sent successfully!', response);
+        }, function(error) {
+            alert('Message sending failed. Please try again.');
+            console.log('FAILED...', error);
+        });
 }
