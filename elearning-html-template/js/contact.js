2cdfb310feab07137f9504031464893ee287bf28
jayawickrma <142202862+jayawickrma@users.noreply.github.com>
2024-10-01 22:47:48 +0530

    updated

--- elearning-html-template/js/contact.js
+++ elearning-html-template/js/contact.js
@@ -1,9 +1,9 @@
 function sendMail(){
     let params ={
-        name : document.getElementById("name").value,
-        email :document.getElementById("email").value,
-        subject :document.getElementById("subject").value,
-        message:document.getElementById("message").value
+        to_name :document.getElementById("name").value(),
+        from_name :document.getElementById("email").value(),
+        subject :document.getElementById("subject").value(),
+        message :document.getElementById("message").value(),
     }
-    emailjs.send("service_657ab6c","template_v9j5mum",params).then(alert("Email sent"));
+    emailjs.send("service_657ab6c","template_v9j5mum",params).then(alert("Mail sent !!!"));
 }
