4badb85e84179bd5fecddf51a28ab69b4493b8c2
ASUS <nethmidileksha6@gmail.com>
2024-10-01 15:05:22 +0530

    contact.html email sending option done

--- elearning-html-template/js/contact.js
+++ elearning-html-template/js/contact.js
@@ -5,5 +5,5 @@ function sendMail(){
         subject :document.getElementById("subject").value,
         message:document.getElementById("message").value
     }
-    emailjs.send("service_657ab6c","template_v9j5mum",params).then();
+    emailjs.send("service_657ab6c","template_v9j5mum",params).then(alert("Email sent"));
 }
