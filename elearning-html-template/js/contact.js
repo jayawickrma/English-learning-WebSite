8a6c4be71d2d2dae9f79fe891f2ca437b9c2faa7
jayawickrma <142202862+jayawickrma@users.noreply.github.com>
2024-10-05 22:36:09 +0530

    email done contact and apply course

--- elearning-html-template/js/contact.js
+++ elearning-html-template/js/contact.js
@@ -1,9 +1,14 @@
 function sendMail(){
     let params ={
-        name :document.getElementById("name").value(),
-        email :document.getElementById("email").value(),
-        subject :document.getElementById("subject").value(),
-        message :document.getElementById("message").value(),
+        name :document.getElementById("name").value,
+        email :document.getElementById("email").value,
+        subject :document.getElementById("subject").value,
+        message :document.getElementById("message").value
     }
-    emailjs.send("service_b7a8a0s","template_dzovutd",params).then(alert("sent.."));
+    emailjs.send("service_b7a8a0s", "template_dk2ckz9", params)
+        .then(function (response) {
+            alert("SUCCESS! Email sent: " + response.status);
+        }, function (error) {
+            alert("FAILED... Error: " + error);
+        });
 }
